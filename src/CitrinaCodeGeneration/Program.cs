69d66eb06fa9da124bfd62380a03869a7bef0c4a
Arthur Khrabrov <arthur.khrabrov@firstlinesoftware.com>
2018-09-24 08:58:03 +0300

    Add code generation wrapper

--- src/CitrinaCodeGeneration/Program.cs
+++ src/CitrinaCodeGeneration/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Editing;
-using SF = Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
-
-namespace CitrinaCodeGeneration
+﻿namespace CitrinaCodeGeneration
 {
     public class Program
     {
@@ -17,114 +10,12 @@ namespace CitrinaCodeGeneration
 
         public static void Main(string[] args)
         {
-            var workspace = new AdhocWorkspace();
-            var generator = SyntaxGenerator.GetGenerator(workspace, LanguageNames.CSharp);
+            var cg = new CodeGenerator();
 
-            var usingDirectives = generator.NamespaceImportDeclaration("System");
-
-            var lastNameField = generator.FieldDeclaration(TestFieldName,
-                generator.TypeExpression(SpecialType.System_String),
-                Accessibility.Private);
-
-            var firstNameField = generator.FieldDeclaration(OtherFieldName,
-                generator.TypeExpression(SpecialType.System_String),
-                Accessibility.Private);
-
-
-
-            var lastNameProperty = generator.PropertyDeclaration(TestPropertyName,
-                generator.TypeExpression(SpecialType.System_String), Accessibility.Public,
-                getAccessorStatements: new SyntaxNode[]
-                { generator.ReturnStatement(generator.IdentifierName(TestFieldName)) },
-                setAccessorStatements: new SyntaxNode[]
-                { generator.AssignmentStatement(generator.IdentifierName(TestFieldName),
-                generator.IdentifierName("value"))});
-
-            var firstNameProperty = generator.PropertyDeclaration(OtherPropertyName,
-                generator.TypeExpression(SpecialType.System_String),
-                Accessibility.Public,
-                getAccessorStatements: new SyntaxNode[]
-                { generator.ReturnStatement(generator.IdentifierName(OtherFieldName)) },
-                setAccessorStatements: new SyntaxNode[]
-                { generator.AssignmentStatement(generator.IdentifierName(OtherFieldName),
-                generator.IdentifierName("value")) });
-
-            var cloneMethodBody = generator.ReturnStatement(generator.
-                InvocationExpression(generator.IdentifierName("MemberwiseClone")));
-
-            var cloneMethoDeclaration = generator.MethodDeclaration("Clone", null,
-                null, null,
-                Accessibility.Public,
-                DeclarationModifiers.Virtual,
-                new SyntaxNode[] { cloneMethodBody });
-
-            var ICloneableInterfaceType = generator.IdentifierName("ICloneable");
-
-            var cloneMethodWithInterfaceType = generator
-                .AsPublicInterfaceImplementation(
-                    cloneMethoDeclaration,
-                    ICloneableInterfaceType);
-
-            var constructorParameters = new SyntaxNode[]
-            {
-                generator.ParameterDeclaration("testValue",
-                generator.TypeExpression(SpecialType.System_String)),
-                generator.ParameterDeclaration("otherValue",
-                generator.TypeExpression(SpecialType.System_String))
-            };
-
-            var constructorBody = new SyntaxNode[]
-            {
-                generator.AssignmentStatement(generator.IdentifierName(TestFieldName),
-                generator.IdentifierName("testValue")),
-                generator.AssignmentStatement(generator.IdentifierName(OtherFieldName),
-                generator.IdentifierName("otherValue"))
-            };
-
-            var constructor = generator.ConstructorDeclaration("Person",
-                constructorParameters, Accessibility.Public,
-                statements: constructorBody);
-
-            var newAutoProperty = GenerateAutoProperty("Test", "int?");
-
-            var members = new SyntaxNode[]
-            {
-                lastNameField,
-                firstNameField,
-                lastNameProperty,
-                firstNameProperty,
-                newAutoProperty,
-                cloneMethodWithInterfaceType,
-                constructor
-            };
-
-            var classDefinition = generator.ClassDeclaration(
-                "Person", typeParameters: null,
-                accessibility: Accessibility.Public,
-                modifiers: DeclarationModifiers.Abstract,
-                baseType: null,
-                interfaceTypes: new SyntaxNode[] { ICloneableInterfaceType },
-                members: members);
-
-            var namespaceDeclaration = generator.NamespaceDeclaration("MyTypes", classDefinition);
-
-            var newNode = generator.CompilationUnit(usingDirectives, namespaceDeclaration)
-                .NormalizeWhitespace();
-                //.NormalizeWhitespace(indentation: string.Empty, eol: string.Empty);
-
-            Console.WriteLine(newNode.GetText());
-            Console.ReadKey();
-        }
-
-        public static PropertyDeclarationSyntax GenerateAutoProperty(string name, string typeName)
-        {
-            return SF.PropertyDeclaration(SF.ParseTypeName(typeName), SF.Identifier(name))
-                .AddModifiers(SF.Token(SyntaxKind.PublicKeyword))
-                .AddAccessorListAccessors(
-                    SF.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
-                        .WithSemicolonToken(SF.Token(SyntaxKind.SemicolonToken)),
-                    SF.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
-                        .WithSemicolonToken(SF.Token(SyntaxKind.SemicolonToken)));
+            var property = cg.NewProperty("Test1", "string");
+            var newClass = cg.NewClass("TestClass", null, property);
+            var newNamespace = cg.NewNamespace("TestNamespace", newClass);
+            cg.NewSourceFile("test.cs", newNamespace, new[] { "System", "System.Text" });
         }
     }
 }
---
+++ src/CitrinaCodeGeneration/SyntaxNodeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Microsoft.CodeAnalysis;
+
+namespace CitrinaCodeGeneration
+{
+    internal static class SyntaxNodeExtensions
+    {
+        public static SyntaxNode GetFormattedCode(this SyntaxNode node)
+        {
+            return node.NormalizeWhitespace();
+        }
+
+        public static SyntaxNode GetCompactCode(this SyntaxNode node)
+        {
+            return node.NormalizeWhitespace(indentation: string.Empty, eol: string.Empty);
+        }
+
+        public static void WriteFormattedSourceFile(this SyntaxNode node, string filePath)
+        {
+            File.WriteAllText(filePath, node.GetFormattedCode().ToString());
+        }
+
+        public static void WriteCompactSourceFile(this SyntaxNode node, string filePath)
+        {
+            File.WriteAllText(filePath, node.GetCompactCode().ToString());
+        }
+    }
+}
